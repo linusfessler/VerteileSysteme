1339b88c48d8bc1c4a8844d07fb875006b1a0df1
Linus <linusfessler@gmail.com>
2016-10-25 19:51:15 +0200

    Implemented message comparator

--- Assignment 3/VS_fesslerl_Chat/app/src/main/java/ch/ethz/inf/vs/a3/message/MessageComparator.java
+++ Assignment 3/VS_fesslerl_Chat/app/src/main/java/ch/ethz/inf/vs/a3/message/MessageComparator.java
@@ -2,14 +2,20 @@ package ch.ethz.inf.vs.a3.message;
 
 import java.util.Comparator;
 
+import ch.ethz.inf.vs.a3.clock.VectorClock;
+
 /**
  * Message comparator class. Use with PriorityQueue.
  */
 public class MessageComparator implements Comparator<Message> {
 
     @Override
-    public int compare(Message lhs, Message rhs) {
-        return 0;
+    public int compare(Message message1, Message message2) {
+        VectorClock clock1 = new VectorClock();
+        VectorClock clock2 = new VectorClock();
+        clock1.setClockFromString(message1.timestamp);
+        clock2.setClockFromString(message2.timestamp);
+        return clock1.happenedBefore(clock2) ? -1 : 1;
     }
 
 }
